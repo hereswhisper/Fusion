f358a3caa6bad7339236c7519971be4512f81fbd
Whisper <123034075+hereswhisper@users.noreply.github.com>
2023-10-22 19:00:52 -0400

    Version detection!

--- Fusion/dllmain.cpp
+++ Fusion/dllmain.cpp
@@ -1,6 +1,28 @@
 // Fusion (INJECTION)
 #include <Windows.h>
+#include "Memory.h"
+#include "Structs.h"
+Unreal::FString(*GEV)();
 
+namespace Helpers {
+    float GetVersion() {
+        uintptr_t GVAddr = Memory::FindPattern("40 53 48 83 EC 20 48 8B D9 E8 ? ? ? ? 48 8B C8 41 B8 04 ? ? ? 48 8B D3");
+        if ((Memory::FindPattern("48 85 C9 74 2E 53 48 83 EC 20 48 8B D9 48 8B 0D ? ? ? ? 48 85 C9 75 0C") != 0 && Memory::FindPattern("48 8D 0D ? ? ? ? E8 ? ? ? ? E8 ? ? ? ? E8 ? ? ? ? 48 8B D6", true, 3) != 0)) {
+            //Scuffed AF but working
+            if (Memory::FindPattern("48 8B 1D ? ? ? ? ? ? ? 10 4C 8D 4D ? 4C", true, 3) != 0) {
+                return 1.8f;
+            }
+        }
+        else {
+            GEV = decltype(GEV)(GVAddr);
+            std::string FNVer = GEV().ToString();
+
+            FNVer.erase(0, FNVer.find_last_of("-", FNVer.length() - 1) + 1);
+
+            return std::stof(FNVer);
+        }
+    }
+}
 BOOL StartDLL(const LPCWSTR dllPath) {
     HMODULE hModule = LoadLibrary(dllPath);
     if (hModule != NULL)
@@ -21,6 +43,12 @@ BOOL APIENTRY DllMain(HMODULE hModule,DWORD  ul_reason_for_call,LPVOID lpReserve
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        AllocConsole();
+        FILE* Handle;
+        freopen_s(&Handle, "CONOUT$", "w", stdout);
+
+        std::cout << Helpers::GetVersion() << std::endl;
+
         LPCWSTR targetDLLPath = L"Fusion-Test.dll";
         if (StartDLL(targetDLLPath))
         {
