5d07bcd3f611dd1642707d28a513f454f0005252
Whisper <123034075+hereswhisper@users.noreply.github.com>
2023-10-22 18:44:59 -0400

    Test Injector in DLL

--- Fusion/dllmain.cpp
+++ Fusion/dllmain.cpp
@@ -1,17 +1,35 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
+// Fusion (INJECTION)
 #include <Windows.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL StartDLL(const LPCWSTR dllPath) {
+    HMODULE hModule = LoadLibrary(dllPath);
+    if (hModule != NULL)
+    {
+        // Locate the DllMain function address
+        FARPROC DllMainAddress = GetProcAddress(hModule, "DllMain");
+        if (DllMainAddress != NULL)
+        {
+            // Manually call the DllMain function
+            return ((BOOL(WINAPI*)(HMODULE, DWORD, LPVOID))DllMainAddress)(hModule, DLL_PROCESS_ATTACH, NULL);
+        }
+    }
+    return FALSE;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,DWORD  ul_reason_for_call,LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
+        LPCWSTR targetDLLPath = L"Fusion-Test.dll";
+        if (StartDLL(targetDLLPath))
+        {
+            // The DllMain process was started successfully
+        }
+        else
+        {
+            // There was an issue starting the DllMain process
+        }
         break;
     }
     return TRUE;
